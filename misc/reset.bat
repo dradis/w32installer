c6259f61af545d530cab884148fd7a13a4854c1d
Daniel Martin <etd(at)nomejortu.com>
2011-04-25 17:08:33 +0000

    Instead of hard-coding the path to the gem binaries, run Thor through Bundle

--- misc/reset.bat
+++ misc/reset.bat
@@ -4,10 +4,9 @@ title Dradis Framework: Resetting the environment...
 ::If the script doesn't work, uncomment and adjust the following:
 ::set PATH=c:\Ruby187\bin;%PATH%
 
-set PATH=c:\Ruby187\lib\ruby\gems\1.8\bin\;%PATH%
 set RAILS_ENV=production
 set BASE=%~dp0
 cd %BASE%\server\
 
 IF NOT EXIST Gemfile.lock call bundle install
-thor dradis:reset
+bundle exec thor dradis:reset
