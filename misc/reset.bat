c43f991fdc30d035ef3b26f736e9efb67392a6c2
Daniel Martin <etd-github@nomejortu.com>
2011-10-10 14:01:52 +0100

    Make a better use of Bundler options
    * Forget about :testing and :development dependencies.
    * Ensure the DevKit is loaded before calling out to bundle install

--- misc/reset.bat
+++ misc/reset.bat
@@ -8,5 +8,16 @@ set RAILS_ENV=production
 set BASE=%~dp0
 cd %BASE%\server\
 
-IF NOT EXIST Gemfile.lock call bundle install
+IF NOT EXIST Gemfile.lock goto installgems
+goto end
+
+:installgems
+:: Include the Ruby DevKit in the path
+call "%BASE%\DevKit\devkitvars.bat"
+
+:: Install dependencies
+call bundle install --without=test development
+
+:end
 bundle exec thor dradis:reset
+rem end
